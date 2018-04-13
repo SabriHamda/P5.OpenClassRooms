315a6b9d6a3e21a9e3282be898906bd78e261fca
sabri <sabri@hamda.ch>
2018-04-13 14:39:42 +0200

    create google.translate.js, this function check the browser language and instantly translate the content in the browser language

--- public/assets/js/google.translate.js
+++ public/assets/js/google.translate.js
@@ -2,6 +2,10 @@
 		
 	
 	'use strict';
+	function browserLang(){
+		var userLang = navigator.language || navigator.userLanguage;
+		return userLang; 
+	}
 
 	function addClassTags(){
 		$( "h1,h2,h3,h4,h5,h6,p,a,span,button" )
@@ -11,13 +15,14 @@
 		}).wrap( "<trans class=\"translate\"> </trans>" );
 	};
 
-	function translateTagsContent(){
+	function translateTagsContent(lang){
+
 		var elementsTr = $('.translate');
 		elementsTr.each(function() {
 			var myData = $(this).html();
 			var mine = $(this);
 			
-				$.get("index.php?action=translate&data=" + myData + "&lang=ja", function(data) {
+				$.get("index.php?action=translate&data=" + myData + "&lang=" + lang, function(data) {
 					console.log(data);
 					mine.text(data);
 					
@@ -26,14 +31,23 @@
 			
 			});
 	}
-	$('#myLoader').css({'display':'block'});
-	addClassTags();
-	translateTagsContent();
 
-	setTimeout(function()
-	{
-		$("#myLoader").css({'display':'none'});
-	}, 5000);
+	function translateContent(){
+		if (browserLang() == 'fr') {
+
+		} else {
+			$('#myLoader').css({'display':'block'});
+			addClassTags();
+			translateTagsContent(browserLang());
+
+			setTimeout(function()
+			{
+				$("#myLoader").css({'display':'none'});
+			}, 5000);
+		}
+	}
+
+	translateContent();
 
 
 }( jQuery ));
