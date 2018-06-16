9708e8c5092fdaa236b6ec9cd47875a592539566
sabri <sabri@hamda.ch>
2018-06-16 20:51:29 +0200

    Pagination Class paginate any table

--- src/Tools/Pagination.php
+++ src/Tools/Pagination.php
@@ -7,16 +7,29 @@
  */
 
 namespace src\Tools;
+
 use src\Repository\Model;
 
 
 class Pagination extends Model
 {
-    public $id, $title,$image,$content,$content_right,$created_at,$updated_at;
-    public $page;
-    private $table;
-    public $countPages;
 
+    /**
+     * @var
+     */
+    public $page;
+    /**
+     * @var
+     */
+    private $table;
+    /**
+     * @var
+     */
+    private $countPages;
+
+    /**
+     * Pagination constructor.
+     */
     public function __construct()
     {
         if ($this->image) {
@@ -25,7 +38,32 @@ class Pagination extends Model
         return null;
     }
 
-    public function run($table, $resultsInPage,$page)
+    public function __sleep()
+    {
+        return array('id', 'title', 'content');
+    }
+
+    public function __wakeup()
+    {
+        $this->__construct();
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCountPages()
+    {
+        return $this->countPages;
+    }
+
+
+    /**
+     * @param $table
+     * @param $resultsInPage
+     * @param $page
+     * @return array
+     */
+    public function run($table, $resultsInPage, $page)
     {
         $this->countPages = $this->countPages($table, $resultsInPage);
         $this->page = $this->getPage($page);
@@ -33,46 +71,57 @@ class Pagination extends Model
 
     }
 
-
+    /**
+     * @param string $table
+     * @param int $resultsInPage
+     * @return float
+     */
     private function countPages(string $table, int $resultsInPage)
     {
-
-        //Une connexion SQL doit être ouverte avant cette ligne...
         $connection = $this->getDb()->getConnection();
         $queryString = "SELECT COUNT(*) AS total FROM `$table`";
-        $countRows = $connection->prepare($queryString); //Nous récupérons le contenu de la requête dans $countRows
+        $countRows = $connection->prepare($queryString);
         $countRows->execute();
         $countRows->setFetchMode(\PDO::FETCH_CLASS, self::class);
-        $totalData = $countRows->fetch(); //On range retour sous la forme d'un tableau.
-        $totalRows = $totalData->total; //On récupère le total pour le placer dans la variable $total.
-        //Nous allons maintenant compter le nombre de pages.
-        $countPages = ceil($totalRows / $resultsInPage);
+        $totalData = $countRows->fetch();
+        $totalRows = $totalData->total;
+        $countPages = ceil($totalRows / $resultsInPage); /** Count how many pages */
         return $countPages;
     }
 
+    /**
+     * @param int $curentPage
+     * @return int
+     */
     private function getPage(int $curentPage)
     {
 
-        if (isset($curentPage) && !empty($curentPage)) // Si la variable $_GET['page'] existe...
+        if (isset($curentPage) && !empty($curentPage))
         {
             $this->page = intval($curentPage);
 
-            if ($this->page > $this->countPages) // Si la valeur de $this->page (le numéro de la page) est plus grande que $this->countPages...
+            if ($this->page > $this->countPages)
             {
                 $this->page = $this->countPages;
             }
         } else // Sinon
         {
-            $this->page = 1; // La page actuelle est la n°1
+
+            $this->page = 1; /** The actual page is the n°1 */
         }
         return $this->page;
     }
 
+    /**
+     * @param string $table
+     * @param int $resultsInPage
+     * @return array
+     */
     private function getTable(string $table, int $resultsInPage)
     {
 
-        $firstEntry = ($this->page - 1) * $resultsInPage; // On calcul la première entrée à lire
-        // La requête sql pour récupérer les messages de la page actuelle.
+        $firstEntry = ($this->page - 1) * $resultsInPage; /** Calculate the first entry to read */
+
         $connection = $this->getDb()->getConnection();
         $queryString = "SELECT * FROM `$table` ORDER BY id DESC LIMIT :firstEntry, :resultsInPage";
         $retour_messages = $connection->prepare($queryString);
@@ -80,7 +129,9 @@ class Pagination extends Model
         $retour_messages->bindValue(':resultsInPage', $resultsInPage, \PDO::PARAM_INT);
         $retour_messages->execute();
         $retour_messages->setFetchMode(\PDO::FETCH_CLASS, self::class);
-        return $retour_messages->fetchAll();
+        dd($retour_messages->fetchAll());
+
+
 
     }
 }
