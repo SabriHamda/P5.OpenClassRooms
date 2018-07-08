d2ee566c7ba658f62efabdacae473f9055e11a90
sabri <sabri@hamda.ch>
2018-07-08 19:13:28 +0200

    Add User model

---
+++ src/Models/User.php
@@ -0,0 +1,142 @@
+<?php
+/**
+ * Created by Sabri Hamda.
+ * Date: 08.07.18
+ * Time: 18:49
+ */
+
+namespace src\Models;
+
+
+class User
+{
+    protected $id;
+    protected $role;
+    protected $name;
+    protected $password;
+    protected $email;
+    protected $civility;
+    protected $register_date;
+    protected $updated_date;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCivility()
+    {
+        return $this->civility;
+    }
+
+    /**
+     * @param mixed $civility
+     */
+    public function setCivility($civility)
+    {
+        $this->civility = $civility;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param mixed $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRegisterDate()
+    {
+        return $this->register_date;
+    }
+
+    /**
+     * @param mixed $register_date
+     */
+    public function setRegisterDate($register_date)
+    {
+        $this->register_date = $register_date;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRole()
+    {
+        return $this->role;
+    }
+
+    /**
+     * @param mixed $role
+     */
+    public function setRole($role)
+    {
+        $this->role = $role;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUpdatedDate()
+    {
+        return $this->updated_date;
+    }
+
+    /**
+     * @param mixed $updated_date
+     */
+    public function setUpdatedDate($updated_date)
+    {
+        $this->updated_date = $updated_date;
+    }
+
+}
