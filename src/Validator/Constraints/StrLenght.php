cc0d3bc2b5d131133f80a49ebb44129ff804adfa
sabri <sabri@hamda.ch>
2018-07-08 19:28:13 +0200

    Validation function to define lenght of string

---
+++ src/Validator/Constraints/StrLenght.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Created by Sabri Hamda.
+ * Date: 08.07.18
+ * Time: 19:15
+ */
+
+namespace src\Validator\Constraints;
+
+
+class StrLenght
+{
+    /**
+     * @var
+     */
+    private $message;
+
+    /**
+     * @param $item
+     * @param $key
+     * @return string|void
+     */
+    private function arrayStrLenght($item, $key)
+    {
+        if (strlen($item) > 5 && strlen($item) < 22) {
+            return;
+        } else {
+            return $this->message[] = $key . ' warning MIN 6 characters and MAX 22';
+        }
+    }
+
+    /**
+     * @param $entry
+     * @return string|void
+     */
+    private function varStrLenght($entry)
+    {
+        if (strlen($item) > 5 && strlen($item) < 22) {
+            return;
+        } else {
+            return $this->message = " warning MIN 6 characters and MAX 22";
+        }
+    }
+
+    /**
+     * @param $entry
+     */
+    public function check($entry)
+    {
+        if (is_array($entry)) {
+            foreach ($entry as $element => $key) {
+                $containSubmit = stristr($element, 'submit');
+            }
+            unset($entry[$containSubmit]);
+            array_walk_recursive($entry, array($this, 'arrayStrLenght'));
+        } else {
+
+            $this->varStrLenght($entry);
+        }
+
+
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMessage()
+    {
+        return $this->message;
+    }
+}
